1f98e6ed11dfa44584a93179e3c95c134c69803e
unknown <3200283+Nanges@users.noreply.github.com>
2020-02-29 00:54:48 +0100

    Add smile page (en/fr)

---
+++ app/views/pages/en/smile.php
@@ -0,0 +1,49 @@
+<?php 
+		$this->layout("/layout/main_en", [
+			"tab_title" => "Brussels Eye Center - S.M.I.L.E",
+			"title" => "Eye treatments"
+		]); 
+	?>
+
+<h2 class='text-center'>REFRACTIVE SURGERY</h2>
+<h3 class='fs-12'>S.M.I.L.E (SMall Incision Lenticule Extraction)</h3>
+
+<p>Is a technic using a Femto Second laser system, using high precision and speed to reshape the
+cornea.</p>
+<p>The laser creates a small lense shaped piece of corneal tissue known as lenticule inside the
+cornea.</p>
+<p>The laser time of the surgery is about 25 seconds. Then, the surgeon removes the lenticule,
+across a small incision at the front part of the eye. This reshapes the corneal and corrects the
+refractive errors.</p>
+<p>SMILE is indicated in the case of treatment of myopia and low astigmatism.</p>
+
+<h4 class='fs-12'>BENEFITS</h4>
+<ul>
+	<li>Minimally invasive</li>
+	<li>The procedure is performed with a 2-4 mm wide incision on the surface of the eye.</li>
+	<li>Less post-operative dryness</li>
+</ul>
+
+<h4 class='fs-12'>DISADVANTAGES</h4>
+<ul>
+	<li>SMILE is not yet able to treat hyperopia and high astigmatism.</li>
+	<li>The vision recovery is slower than Lasik. It takes several days to get a 20/20 vision, while with
+Lasik a 20/20 vision is obtained the following day after the surgery.</li>
+	<li>SMILE needs more anti inflammatory treatment.</li>
+	<li>The retreatment of over or under correction after SMILE is more complicated than Lasik.</li>
+</ul>
+
+<h4 class='fs-12'>COMPLICATIONS</h4>
+<p>The complications are the same than other refractive surgeries (infections, night vision
+problems…) excluding flap dislocation or folds.</p>
+
+
+<?php $this->start("banner") ?>
+	<img src="../images/top_docteurs.jpg" alt="Brussels Eye Center, Eye Center specialized in medical and surgical treatment of the eyes" width="600" height="250">
+<?php $this->stop() ?>
+
+<?php $this->start("top3") ?>
+	<h2>Brussels Eye Center</h2>
+	<p>In the heart of Brussels, in the European  Community district, the Eye Center is specialized in prevention, medical and  surgical treatment of the eyes.</p>
+	<p>Our center gathers 7 experienced ophthalmologists.</p>
+<?php $this->stop() ?>
---
+++ app/views/pages/smile.php
@@ -0,0 +1,46 @@
+<?php 
+		$this->layout("/layout/main_fr", [
+			"tab_title" => "Brussels Eye Center - S.M.I.L.E",
+			"title" => "Soins oculaires"
+		]); 
+	?>
+
+<h2 class='text-center'>LA CHIRURGIE RÉFRACTIVE</h2>
+<h3 class="fs-12">S.M.I.L.E (SMall Incision Lenticule Extraction)</h3>
+<p>Il s’agit d’une nouvelle technique chirurgicale de la myopie et de l’astigmatisme par Femto
+Second laser. Elle s’effectue sous anesthésie locale par gouttes.</p>
+<p>Le laser prédécoupe un petit lenticule dans l’épaisseur de la cornée correspondant au volume
+de la myopie et de l’astigmatisme à traiter.</p>
+<p>Le laser réalise une petite incision périphérique (de 2 à 4 mm de largeur) à travers laquelle le
+chirurgien va disséquer le lenticule et de l’extraire de la cornée.</p>
+<p>Les résultats de cette technique dans le cas de la myopie et de l’astigmatisme sont
+prédictibles et entrainent de très bons résultats réfractifs.</p>
+
+<h4 class='fs-12'>AVANTAGES<!-- de la technique SMILE par rapport au Lasik et autres techniques--></h4>
+<ul>
+	<li>Absence de réalisation d’un capot fragile en cas de frottemens et de traumatisme.</li>
+	<li>Moindre sécheresse</li>
+</ul>
+
+<h4 class='fs-12'>INCONVENIENTS<!--de la technique SMILE par rapport au Lasik--></h4>
+<ul>
+	<li>Récupération visuelle plus lente.</li>
+	<li>Vision optimale après quelques jours et non pas le lendemain comme le Lasik.</li>
+	<li>Nécessité de traitement anti-inflammatoires de plus longue durée.</li>
+	<li>Le SMILE contrairement au Lasik ne traite pas pour le moment les forts astigmatismes et l’hypermétropie.</li>
+	<li>Difficulté de retraitement en cas de sur et de sous correction.</li>
+</ul>
+
+<h4 class='fs-12'>COMPLICATIONS</h4>
+<p>Les complications sont identiques aux chirurgies réfractives par Laser (infections,
+inflammation, troubles de vision nocturnes…) hormi les plis et le déplacement du capot.</p>
+
+<?php $this->start("banner") ?>
+	<img src="../images/top_docteurs.jpg" alt="Brussels Eye Center, Eye Center specialized in medical and surgical treatment of the eyes" width="600" height="250">
+<?php $this->stop() ?>
+
+<?php $this->start("top3") ?>
+    <h2>Brussels Eye Center</h2>
+	<p>En plein cœur de Bruxelles, dans le quartier des Communautés Européennes, le Centre d’Ophtalmologie spécialisé dans la prévention, le traitement médical et chirurgical des yeux.</p>
+	<p>Notre centre regroupe 7 ophtalmologues expérimentés dans les maladies et la chirurgie des yeux.</p>
+<?php $this->stop() ?>
